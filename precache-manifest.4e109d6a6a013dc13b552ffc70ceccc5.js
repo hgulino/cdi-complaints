831963409a1867ee80a31bb6b73726c9145cbe08
Hunter <huntergulino@gmail.com>
2019-11-10 02:52:13 -0800

    Updates

---
+++ precache-manifest.4e109d6a6a013dc13b552ffc70ceccc5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdfe1aefed0fbe723374c625b12a283c",
+    "url": "/hgulino/cdi-complaints/index.html"
+  },
+  {
+    "revision": "6f969cf10b4675726d81",
+    "url": "/hgulino/cdi-complaints/static/js/2.3eb5047d.chunk.js"
+  },
+  {
+    "revision": "36a862be52f7b2e8fd24",
+    "url": "/hgulino/cdi-complaints/static/js/main.876119b2.chunk.js"
+  },
+  {
+    "revision": "c3a45523d18886431db8",
+    "url": "/hgulino/cdi-complaints/static/js/runtime~main.3188b3f2.js"
+  }
+]);
